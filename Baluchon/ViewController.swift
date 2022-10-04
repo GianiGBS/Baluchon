afbc154d8b9920b6318bd81507f18761218f4411
GiovanniGBS <giovannigbs@icloud.com>
2022-10-04 17:16:54 -0400

    Initial Commit

---
+++ Baluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baluchon
+//
+//  Created by Giovanni Gabriel on 04/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
